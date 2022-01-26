140e93c8bbf9b390bb57e6106d4bafa445171d71
chocoshop <15ec112j@rikkyo.ac.jp>
2022-01-26 23:56:08 +0900

    引数が変わったので呼び出し元を修正

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import routes from './routes';
 const port = 80;
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-    const route =  new Route(req, res, routes);
+    const route =  new Route(req.url, routes);
     const action = route.initilize();
     action.then(({controller, method}) => {
         controller[method](req, res);
