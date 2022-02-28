1a823c03cd1208efa988bdc86e5e8a0b3fdc8994
chocoshop <15ec112j@rikkyo.ac.jp>
2022-03-01 01:21:19 +0900

    typeをimplementsする作りに変えたので不要になった

--- modules/Controllers/index.ts
+++
@@ -1,7 +0,0 @@
-export default class Controller<K> implements Contracts.Controller<K> {
-    constructor() {   
-    }
-
-    // メソッドを1つ持たない場合に呼び出す対象がわからずエラーになる模様。controller['method'] => error
-    __invoke() {}
-}
