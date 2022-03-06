b93fbcba32256e90ff0c4b47a2c1756926a365f9
chocoshop <15ec112j@rikkyo.ac.jp>
2022-03-06 23:39:41 +0900

    requestとresponseのオブジェクトをコントローラーの引数に渡せるようにした
    コントローラーがロードできない場合は直接例外を投げるようにした

--- modules/Route/index.ts
+++ modules/Route/index.ts
@@ -1,27 +1,34 @@
+import * as http from 'http';
 import { RouteResolver } from './../Resolver/RouteResolver';
 import Action from '../Http/Action';
 import NotFoundError from '../Error/NotFoundError';
 
 export default class Route {
     private url: string;
+    private req: http.IncomingMessage;
+    private res: http.ServerResponse;
     private resolver: RouteResolver;
 
-    constructor(url: string, resolver: RouteResolver) {
-        this.url = url;
+    constructor(req: http.IncomingMessage, res: http.ServerResponse, resolver: RouteResolver) {
+        if (req.url === undefined) {
+            throw new NotFoundError('Invalid Request');
+        }
+        this.url = req.url;
+        this.req = req;
+        this.res = res;
         this.resolver = resolver;
     }
 
     async exec(): Promise<TController<Methods>|void> {
+
         const resolver = await this.resolver.resolve(this.url);
         const action = resolver.getAction();
         
         if (action === null) {
             throw new Error('Given Action is null')
         }
+
         const controller = await this.getControllerInstance(resolver.getControllerPath());
-        if (controller === null) {
-            throw new NotFoundError('Could not load Controller');
-        }
         
         return this.execController(action, controller);
     }
@@ -29,17 +36,17 @@ export default class Route {
     async execController(action: Action, controller: TController<Methods>): Promise<TController<Methods>|void> {
         const method = action.getMethod();
         if (this.isMethodExist(controller, method)) {
-            return controller[method]();
+            return controller[method](this.req, this.res);
         }
     }
 
     // 型安全に書く方法がわからない..読み込むモジュールがany型になるのと、default exportかそうでないかの区別がつかない
-    async getControllerInstance(path: string): Promise<TController<Methods>|null> {
+    async getControllerInstance(path: string): Promise<TController<Methods>> {
         try {
             const controller = await import(path);
             return new controller.default();
         } catch(e) {
-            return null;
+            throw new NotFoundError('Could not load Controller');
         }
     }
 
--- modules/tests/Route/index.test.ts
+++ modules/tests/Route/index.test.ts
@@ -1,3 +1,4 @@
+import * as http from 'http';
 import { RouteResolver } from './../../Resolver/RouteResolver';
 import Action from '../../Http/Action';
 import Route from '../../Route';
@@ -5,43 +6,28 @@ import TestController from './TestController';
 
 describe('getControllerInstance', () => {
     test('Controllerのインスタンスを返す', async () => {
-        const route = new Route('/test', new RouteResolver());
+        const route = new Route({url: '/test'} as http.IncomingMessage, {} as http.ServerResponse, new RouteResolver());
         expect(route.getControllerInstance('/var/www/modules/tests/Route/TestController')).resolves.toBeInstanceOf(TestController);
     });
 
-    test('コントローラーのファイルをimportできない場合は、nullを返す', async () => {
-        const route = new Route('/test', new RouteResolver());
-        expect(route.getControllerInstance('/var/www/modules/tests/Route/DummyController')).resolves.toBe(null);
+    test('コントローラーのファイルをimportできない場合は、例外を投げる', async () => {
+        const route = new Route({url: '/test'} as http.IncomingMessage, {} as http.ServerResponse, new RouteResolver());
+        expect(route.getControllerInstance('/var/www/modules/tests/Route/DummyController')).rejects.toThrow(new Error('Could not load Controller'));
     });
 });
 
 describe('isMethodExist', () => {
     test('存在するメソッドの場合は、trueを返す', () => {
-        const route = new Route('/test', new RouteResolver());
+        const route = new Route({url: '/test'} as http.IncomingMessage, {} as http.ServerResponse, new RouteResolver());
         expect(route.isMethodExist(new TestController(), 'index')).toBe(true);
     });
     
     test('存在しないメソッドの場合は、falseを返す', () => {
-        const route = new Route('/test', new RouteResolver());
+        const route = new Route({url: '/test'} as http.IncomingMessage, {} as http.ServerResponse, new RouteResolver());
         expect(route.isMethodExist(new TestController(), 'notfound')).toBe(false);
     })
 });
 
-test('コントローラーのインスタンスがnullの場合は、例外を投げる', async () => {
-    jest.spyOn(RouteResolver.prototype, 'resolve')
-        .mockResolvedValue(new Promise(() => {
-            return {
-                getAction() {
-                    return new Action('TestController@index');
-                }
-            }
-        }));
-    jest.spyOn(Route.prototype, 'getControllerInstance')
-        .mockReturnValue(new Promise(() => null));
-    
-    const route = new Route('', new RouteResolver());
-    expect(() => route.exec()).rejects.toThrow(new Error('Could not load Controller'));
-});
 
 test('アクションがnullの場合は、例外を投げる', async () => {
     jest.spyOn(RouteResolver.prototype, 'resolve')
@@ -51,6 +37,6 @@ test('アクションがnullの場合は、例外を投げる', async () => {
                     return null;
             }}
         }));
-    const route = new Route('', new RouteResolver());
+    const route = new Route({url: '/test'} as http.IncomingMessage, {} as http.ServerResponse, new RouteResolver());
     expect(() => route.exec()).rejects.toThrow(new Error('Given Action is null'));
 });
