7d33178c5b2b6a424d1bd0e0e89a85b1c5632a78
chocoshop <15ec112j@rikkyo.ac.jp>
2022-03-08 00:34:16 +0900

    引数の型にhttpのプロパティを利用しない場合に型エラーを起こすようにした。
    実装側のコントローラーではなく元の型定義で縛れるように変更したいが一度この状態でcommit

--- App/Http/IndexController.ts
+++ App/Http/IndexController.ts
@@ -1,8 +1,11 @@
 import * as http from 'http';
 
-export default class IndexController implements TController<IndexController>
+export default class IndexController implements TController<{
+    getA: (req: http.IncomingMessage, res: http.ServerResponse) => http.ServerResponse;
+}>
 {
     getA(req: http.IncomingMessage, res: http.ServerResponse): http.ServerResponse {
         return res.end('aにきた');
     }
+
 }
--- modules/@types/index.d.ts
+++ modules/@types/index.d.ts
@@ -6,6 +6,8 @@ type Methods = {
     invoke?,
 }
 
+type ControllerMethod = (req: http.IncomingMessage, res: http.ServerResponse) => http.ServerResponse;
+
 type TController<T> = {
     [P in keyof T]: T[P];
 }
