da1b8408715fc347ec3111a4063b07db1056eaa5
chocoshop <15ec112j@rikkyo.ac.jp>
2022-03-13 23:56:02 +0900

    静的なファイルを返せるようにした

--- server.ts
+++ server.ts
@@ -1,17 +1,29 @@
-import { document } from './modules/View/Document';
+import { isAsset } from './modules/Route/static/utils/index';
 import * as http from 'http';
 import Handler from './modules/Error/Handler';
 import { RouteResolver } from './modules/Resolver/RouteResolver';
 import Route from './modules/Route';
-import { Test } from './test';
+import NotFoundError from './modules/Error/NotFoundError';
+import StaticRoute from './modules/Route/static';
 
 const port = 80;
 
 const server = http.createServer(async (req: http.IncomingMessage, res: http.ServerResponse) => {
     try {
+        if (!req.url) {
+            throw new NotFoundError('Invalid Route');
+        }
+
+        if (isAsset(req.url)) {
+            const file = await StaticRoute.exec(req.url);
+            res.writeHead(200, {'Content-Type': `${file.contentType}; charset=utf-8`});
+            res.end(file.content);
+            return;
+        };
+
         const route =  new Route(req, res, new RouteResolver());
-        // const data = await route.exec();
-        res.end(document(Test));
+        const view = await route.exec();
+        res.end(view);
     } catch (e) {
         (new Handler()).render(res, e);
     }
