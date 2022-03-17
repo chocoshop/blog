d6847bf6c3466d451d8c408b804e06c31efd9de9
chocoshop <15ec112j@rikkyo.ac.jp>
2022-03-17 19:39:35 +0900

    pagesの下にあるファイルをビルドしてpublicに吐けるようにした

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const glob = require('glob');
+
+module.exports = {
+    mode: 'development',
+    entry: Object.fromEntries(glob.sync('./pages/**/**.tsx').map(pathInfo => [path.dirname(pathInfo), path.resolve('.', pathInfo)])),
+    output: {
+        path: path.join(__dirname, 'public'),
+        // 本番はhashした値にする
+        // filename: "[name]/[hash].js",
+        filename: "[name]/bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+}
