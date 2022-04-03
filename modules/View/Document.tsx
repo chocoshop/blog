36c76b32e848bce8071d74e0454dd6e6e631bac6
chocoshop <15ec112j@rikkyo.ac.jp>
2022-04-04 00:53:57 +0900

    reset.cssを読み込む

--- modules/View/Document.tsx
+++ modules/View/Document.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 export const Document: React.FC = ({ children }) => {
     return ( 
         <html>
+            <head>
+                <link rel="stylesheet" href="public/css/reset.css" />
+            </head>
             <body>
                 <div id='app'></div>
                 {children}
