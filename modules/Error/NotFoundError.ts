0eb7411eefdee2490c8f9fbb5ce4fb0445c79612
chocoshop <15ec112j@rikkyo.ac.jp>
2022-04-13 01:03:09 +0900

    メッセージの入力は任意にした

--- modules/Error/NotFoundError.ts
+++ modules/Error/NotFoundError.ts
@@ -1,7 +1,7 @@
 export default class NotFoundError extends Error {
     public message: string;
 
-    constructor(message: string) {
+    constructor(message?: string) {
         super();
         this.message = message ?? 'Not Found';
     }
