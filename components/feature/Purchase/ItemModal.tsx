36ab3f079f18679e36a68ca4affceacc49b6c27c
chocoshop <15ec112j@rikkyo.ac.jp>
2022-04-15 02:21:01 +0900

    商品のモーダルを実装した
    クリック時に詳細が見れるようにする

--- components/feature/Purchase/ItemModal.tsx
+++ components/feature/Purchase/ItemModal.tsx
@@ -1,33 +1,30 @@
 import styled from "styled-components";
-import theme from "../variables/theme";
-import { Primary } from "../atoms/Button";
+import { Primary } from "../../atoms/Button";
+import theme from "../../variables/theme";
 
-type Item = {
-    id: number,
-    title: string,
-    price: number,
-    description: string,
-}
-
-export const Card : React.FC<Item> = (item) => {
-    return(
-        <Wrapper theme={theme} key={item.id}>
+export const ItemModal: React.FC<Item> = item => {
+    return (
+        <Wrapper theme={theme} onClick={(e) => {e.stopPropagation()}}>
             <h4>{item.title}</h4>
             <img src="public/images/dummy1.png" alt="ダミー画像" />
             <br />
             <span>¥{item.price}</span>
             <br />
             <p>{item.description}</p>
-            <Primary text="予約"/>
+            <ButtonWrapper>
+                <Primary text="予約を確定する"/>
+            </ButtonWrapper>
         </Wrapper>
     )
 }
-
 
 const Wrapper = styled.div`
     background-color: ${({theme}) => theme.colors.elements.bgColor};
     padding: 1.5rem 2.5rem;
     border-radius: 5px;
+    width: 350px;
+    margin: 0 auto;
+    z-index: 2;
     & > img {
         width: 100%;
     }
@@ -35,8 +32,11 @@ const Wrapper = styled.div`
         font-size: 1.5rem;
     }
     & > p {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
+        overflow-wrap: break-word;
     }
-`
+`;
+
+const ButtonWrapper = styled.div`
+    width: 100%;
+    text-align: right;
+`;
