d650bb09ee3fb2f2db2d31cb83ac5b141e9c58fa
chocoshop <15ec112j@rikkyo.ac.jp>
2022-04-17 22:12:35 +0900

    disabledのボタンを追加

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,17 +1,31 @@
 import styled from 'styled-components';
 import theme from '../variables/theme';
 
-export const Primary: React.FC<{text: string, onClick?: () => void}> = ({text, onClick}) => {
+export const PrimaryButton: React.FC<{text: string, onClick?: () => void}> = ({text, onClick}) => {
     return (
-        <Button theme={theme} onClick={onClick}>{text}</Button>
-    )
+        <Primary theme={theme} onClick={onClick}>{text}</Primary>
+    );
 }
 
-const Button = styled.button`
+export const DisabledButton : React.FC<{text: string}> = ({text}) => {
+    return (
+        <Disabled theme={theme} disabled={true}>{text}</Disabled>
+    );
+}
+
+const Primary = styled.button`
     background: ${({theme}) => theme.colors.elements.primaryBtn};
     color: ${({theme}) => theme.colors.elements.btnText};
     font-weight: bold;
     padding: 0.5rem;
-    border-radius: 7.5px;
+    border-radius: 4px;
     cursor: pointer;
+`;
+
+const Disabled = styled.button`
+    background: ${({theme}) => theme.colors.elements.disabledBtn};
+    color: gray;
+    font-weight: bold;
+    padding: 0.5rem;
+    border-radius: 7.5px;
 `;
